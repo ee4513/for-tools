eec5c8ea8acb03dcf64513f50bb88b5db85b38f7
ee4513 <148230985+ee4513@users.noreply.github.com>
2023-11-08 18:49:37 +0530

    Add files via upload

---
+++ removeGLBtexture.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Check if removeTexCmd.js file exists, download if not
+if not exist "removeTexCmd.js" (
+    echo Downloading removeTexCmd.js...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ee4513/for-tools/raw/main/removeTexCmd.js', 'removeTexCmd.js')"
+
+    REM Check if the file was downloaded successfully
+    if exist "removeTexCmd.js" (
+        echo removeTexCmd.js downloaded successfully.
+    ) else (
+        echo Error downloading removeTexCmd.js. Please check the link or try again later.
+        pause
+        exit /b
+    )
+) else (
+    echo removeTexCmd.js already exists in the current directory.
+)
+
+
+REM Create output directory if not present
+if not exist "no-texture" (
+    mkdir no-texture
+)
+
+REM Loop through all FBX files in the current directory and convert to GLB
+for %%f in (*.glb) do (
+    set "input=%%f"
+    set "output=no-texture\%%~nf.glb"
+    node removeTexCmd.js !input! !output!
+    if !errorlevel! neq 0 (
+        echo Error removing !input! from GLB format.
+    ) else (
+        echo Removed-texture-From !input! to !output!
+    )
+)
+
+echo All texture are removed from GLB.
+pause
